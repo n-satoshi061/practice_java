01413fc876040dba8125354cd9ba2efb6a6094a8
西川智士 <nishikawatomoo@nishikawatomoonoMacBook-Air.local>
2024-11-05 21:43:06 +0900

    if文

--- Cast.java
+++ Cast.java
@@ -1,12 +1,18 @@
 public class Cast {
     public static void main(String[] args) {
         int price = Integer.parseInt(args[0]);
-        double rate = 0.08; // 消費税率: 8%
-        int amount;
+        double rate = 0.10; // 消費税率: 8%
+        int discount, amount;
 
-        System.out.println(price * (1 + rate));
-        amount = (int)(price * (1 + rate));
-        System.out.println("税込金額: " + amount + "円");
-
+        if( price >= 5000) {
+            discount = 500;
+        } else if( price >= 3000) {
+            discount = 300;
+        } else {
+            discount = 0;
+        }
+        amount = (int)((price - discount) * (1 + rate));
+        System.out.println("値引き金額: " + discount + "円");
+        System.out.println("合計金額: " + amount + "円");
     }
 }
