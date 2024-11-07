004284396f7fc160fb15561eafcec4a6dd7da26d
西川智士 <nishikawatomoo@nishikawatomoonoMacBook-Air.local>
2024-11-07 11:43:05 +0900

    オーバーロード

---
+++ Student2.java
@@ -0,0 +1,21 @@
+public class Student2 {
+    String name;
+    int engScore;
+    int mathScore;
+
+    void setData(String n) {
+        name = n;
+    }
+    void setData(String n, int e, int m) {
+        name = n;
+        engScore = e;
+        mathScore = m;
+    }
+    void setScore(int e, int m) {
+        engScore = e;
+        mathScore = m;
+    }
+    void display() {
+        System.out.println(name + "さんの英語の点数は" + engScore + "点、数学の点数は" + mathScore + "点です。");
+    }
+}
