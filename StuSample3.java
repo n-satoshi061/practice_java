e4949f04f87c8b2f760a47bfab6a4bc241e63a9e
西川智士 <nishikawatomoo@nishikawatomoonoMacBook-Air.local>
2024-11-08 11:17:00 +0900

    コンストラクタ

---
+++ StuSample3.java
@@ -0,0 +1,10 @@
+public class StuSample3 {
+    public static void main(String[] args) {
+        Student3 stu1 = new Student3("西川");
+        Student3 stu2 = new Student3("塚田", 75, 100);
+
+        stu1.setScore(90, 80);
+        stu1.display();
+        stu2.display();
+    }
+}
---
+++ Student3.java
@@ -0,0 +1,21 @@
+public class Student3 {
+    String name;
+    int engScore;
+    int mathScore;
+
+    Student3(String n) {
+        name = n;
+    }
+    Student3(String n, int e, int m) {
+        name = n;
+        engScore = e;
+        mathScore = m;
+    }
+    void setScore(int e, int m) {
+        engScore = e;
+        mathScore = m;
+    }
+    void display() {
+        System.out.println(name + "さんの英語の点数は" + engScore + "点、数学の点数は" + mathScore + "点です。");
+    }
+}
