daaac6c17c18e8f8f093503be25a463bfd137ade
西川智士 <nishikawatomoo@nishikawatomoonoMacBook-Air.local>
2024-11-08 12:29:24 +0900

    アクセス修飾子

---
+++ DriveCar3.java
@@ -0,0 +1,11 @@
+public class DriveCar3 {
+    public static void main(String[] args) {
+        Car3 car1 = new Car3();
+        car1.run(30);
+        car1.display();
+
+        Car3 car2 = new Car3(2525);
+        car2.run(50);
+        car2.display();
+    }
+}
