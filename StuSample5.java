5489e4be705b11e2d9f6f49429886f3355d34995
西川智士 <n.satoshi061@gmail.com>
2024-11-08 15:34:47 +0900

    アクセス修飾子

---
+++ StuSample5.java
@@ -0,0 +1,12 @@
+public class StuSample5 {
+    public static void main(String[] args) {
+        Student5 stu1 = new Student5("西川");
+        stu1.setScore(80);
+        stu1.display();
+
+        Student5 stu2 = new Student5("塚田");
+        stu2.setScore(-50);
+        // stu2.score = -50;
+        stu2.display();
+    }
+}
