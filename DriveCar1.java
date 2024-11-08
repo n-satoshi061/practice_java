7c878e0d05df39e8990fae3f19d5af0d5170d036
西川智士 <nishikawatomoo@nishikawatomoonoMacBook-Air.local>
2024-11-08 12:27:53 +0900

    オーバーロード

---
+++ DriveCar1.java
@@ -0,0 +1,10 @@
+public class DriveCar1 {
+    public static void main(String[] args) {
+        Car1 car1 = new Car1();
+        car1.setNo(2525);
+        car1.run(30);
+        car1.display();
+        car1.brake(10);
+        car1.display();
+    }
+}
