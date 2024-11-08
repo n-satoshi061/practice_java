f823b401e230c25731ac33b4d9c6cc81be712640
西川智士 <nishikawatomoo@nishikawatomoonoMacBook-Air.local>
2024-11-08 11:40:56 +0900

    static

---
+++ StuSample4.java
@@ -0,0 +1,10 @@
+public class StuSample4 {
+    public static void main(String[] args) {
+        Student4.display();
+        Student4 stu1 = new Student4("西川");
+        Student4.display();
+
+        Student4 stu2 = new Student4("塚田");
+        Student4.display();
+    }
+}
