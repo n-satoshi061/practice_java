29eec80e33b692fcd1122683f727cca5701a5e4b
西川智士 <n.satoshi061@gmail.com>
2024-11-09 14:57:09 +0900

    型変換

---
+++ Lesson17/StuSample.java
@@ -0,0 +1,18 @@
+package Lesson17;
+public class StuSample {
+    public static void main(String[] args) {
+        Student stu1 = new Student("西川", 1);
+        Person psn = stu1;
+        psn.display();
+        // psn.chgStuNo(1001);
+
+        /* 補足: instanceofキーワード
+           オブジェクトのクラスを特定する
+           対象オブジェクト instanceof クラス名 */
+           if(psn instanceof Student) {
+            Student stu2 = (Student)psn;
+            stu2.chgStuNo(1001);
+            stu2.display();
+           }
+    }
+}
