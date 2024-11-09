4a55ecaaddbe74ec3af650af1006c8b23a033059
西川智士 <n.satoshi061@gmail.com>
2024-11-09 17:20:27 +0900

    パッケージ

---
+++ Lesson20/StuSample.java
@@ -0,0 +1,10 @@
+package Lesson20;
+
+import Lesson20.pack.Student;
+
+public class StuSample {
+    public static void main(String[] args) {
+        Student stu = new Student("西川");
+        stu.display();
+    }
+}
---
+++ Lesson20/pack/Student.java
@@ -0,0 +1,12 @@
+package Lesson20.pack;
+
+public class Student {
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+    public void display() {
+        System.out.println("名前: " + name);
+    }
+}
