856489df3da2e5aa1998224ebdd54a48bef5db16
西川智士 <n.satoshi061@gmail.com>
2024-11-09 13:36:22 +0900

    演習

---
+++ Lesson16/Car1.java
@@ -0,0 +1,12 @@
+package Lesson16;
+
+public class  Car1 {
+    private int no;
+
+    public void setNo(int n) {
+        no = n;
+    }
+    public void display() {
+        System.out.println("ナンバーは" + no);
+    }
+}
---
+++ Lesson16/DriveTaxi.java
@@ -0,0 +1,11 @@
+package Lesson16;
+
+public class DriveTaxi {
+    public static void main(String[] args) {
+        Taxi1 taxi1 = new Taxi1();
+        taxi1.setNo(2525);
+        taxi1.start();
+        taxi1.run();
+        taxi1.display();
+    }
+}
