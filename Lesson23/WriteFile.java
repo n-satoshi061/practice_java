3b83cd239e58aa7a7e46a6dda9b977a0c2233ece
西川智士 <n.satoshi061@gmail.com>
2024-11-10 16:38:01 +0900

    throw throws

---
+++ Lesson23/WriteFile.java
@@ -0,0 +1,15 @@
+package Lesson23;
+
+import java.io.IOException;
+
+public class WriteFile {
+    public void open() {
+        System.out.println("ファイルを開きます");
+    }
+    public void write() throws IOException {
+        throw new IOException();
+    }
+    public void close() {
+        System.out.println("ファイルを保存して閉じます");
+    }
+}
