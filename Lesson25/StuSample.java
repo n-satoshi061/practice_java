c50c9787180784ad6c8e38472436f635b5f7bc00
西川智士 <n.satoshi061@gmail.com>
2024-11-12 14:59:39 +0900

    equalsメソッド

---
+++ Lesson25/StuSample.java
@@ -0,0 +1,13 @@
+package Lesson25;
+
+public class StuSample {
+    public static void main(String[] args) {
+        Student stu1 = new Student("西川");
+        Student stu2 = new Student("西川");
+
+        boolean result = stu1.equals(stu2);
+        System.out.println(result);
+        System.out.println(stu1);
+
+    }
+}
