e9249669bfb8e67738fc19c0479fc5552ef7244a
西川智士 <n.satoshi061@gmail.com>
2024-11-30 11:35:07 +0900

    webアプリからDB接続

---
+++ tomcat/webapps/dbweb/initialize.sql
@@ -0,0 +1,11 @@
+DELETE FROM student;
+INSERT INTO student VALUES(1,'西川',60);
+INSERT INTO student VALUES(2,'塚田',100);
+INSERT INTO student VALUES(3,'山田',85);
+INSERT INTO student VALUES(4,'田中',90);
+INSERT INTO student VALUES(5,'太田',75);
+INSERT INTO student VALUES(6,'菅原',95);
+INSERT INTO student VALUES(7,'鈴木',90);
+INSERT INTO student VALUES(8,'横浜',85);
+INSERT INTO student VALUES(9,'坂東',80);
+INSERT INTO student VALUES(10,'奈良',75);
